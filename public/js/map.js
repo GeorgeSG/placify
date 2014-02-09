7ec7ef00745aac7db0830a9b01e878180c50fcd3
Georgi Gardev <georgi@gardev.com>
2014-02-09 23:26:04 +0200

    Add Modal for adding a point

--- public/js/map.js
+++ public/js/map.js
@@ -16,6 +16,9 @@ mapOptions = {
   zoom: 12
 };
 
+// Call initial map-loading logic
+_m.event.addDomListener(window, 'load', loadMap);
+
 /**
  * Called when the DOM is loaded.
  * Initializes the map and loads all points
@@ -26,64 +29,93 @@ function loadMap() {
 
   // Setup the click event listener for the Map:
   // Add a new marker on the clicked location
-  _m.event.addListener(map, 'click', function(event) {
-    // TODO: create a UI to set Point Details
-    // TODO: call backend services to store the Point
-    var marker = placeMarker(map, event.latLng, "New Point", true);
-  });
+  _m.event.addListener(map, 'click', addNewPoint);
 
   // Create the DIV to hold the control and call the
   // HomeControl() constructor passing in this DIV.
   var homeControlDiv = document.createElement('div');
-  var homeControl = new HomeControl(homeControlDiv, map, home);
+  var homeControl    = new HomeControl(homeControlDiv, map, home);
 
   homeControlDiv.index = 1;
   map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
 
-  // Get All Public Points and load them as markers
-  // on the Map.
+  // Get All Public Points and load them as markers on the Map.
   // TODO: Call backend services
   $.getJSON("/mock/points.json", function(json) {
       loadMarkers(json.markers);
   }).fail(function() {
     console.log('Failed Loading JSON!');
   });
-
-  /**
-   * Loads a set of Points represented as JSON and
-   * displays them on the map.
-   *
-   * Groups markers by type and adds them to the markers hash
-   */
-  function loadMarkers(jsonMarkers) {
-    var types = $.map(jsonMarkers, function(marker, _) { return marker.type; });
-    types     = $.unique(types);
-
-    $.each(types, function(_, type) { markers[type] = [] });
-
-    $.each(jsonMarkers, function(_, element) {
-      var position = latLng(element.lat, element.lng);
-      var marker   = placeMarker(map, position, element.name, true);
-      markers[element.type].push(marker);
-
-      _m.event.addListener(marker, 'click', function() {
-        infoWindow.close();
-        infoWindow.setContent(element.description)
-        infoWindow.open(map, marker);
-      });
-
-      _m.event.addListener(marker, 'dragend', function() {
-        // TODO: call backend services
-        console.log('marker position changed to:');
-        console.log(marker.getPosition());
-      })
-    });
-  }
 }
 
-// Call initial map-loading logic
-_m.event.addDomListener(window, 'load', loadMap);
+/**
+ * Shows a modal to enter new point. If saved,
+ * the point is stored and shown on the map
+ */
+function addNewPoint(event) {
+  var clickEvent = event;
+  var marker = placeMarker(map, clickEvent.latLng, "New Point", true);
 
+  $("#add-point-modal").modal("show");
+
+  $(".discard-point").unbind("click").click(function() {
+    marker.setMap(null);
+  });
+
+  $("#save-point").unbind("click").click(function() {
+    var pointName = $("#new-point-name").val();
+    var pointDescription = $("#new-point-description").val();
+    var pointType = $("#add-point-modal input[name=pointType]:checked").val();
+
+    pointDescription = "<div><h3>" + pointName + "</h3><span>" + pointType + "</span><p>" + pointDescription + "</p></div>";
+
+    marker.setTitle(pointName);
+    addMarker(marker, pointType, pointDescription);
+    // TODO: call backend services to store the point
+  });
+}
+
+/**
+ * Loads a set of Points represented as JSON and
+ * displays them on the map.
+ *
+ * Groups markers by type and adds them to the markers hash
+ */
+function loadMarkers(jsonMarkers) {
+  var types = $.map(jsonMarkers, function(marker, _) { return marker.type; });
+  types = $.unique(types);
+
+  $.each(types, function(_, type) { markers[type] = [] });
+
+  $.each(jsonMarkers, function(_, element) {
+    var position = latLng(element.lat, element.lng);
+    var marker   = placeMarker(map, position, element.name, true);
+
+    addMarker(marker, element.type, element.description);
+  });
+}
+
+function addMarker(marker, type, description) {
+  console.log(markers)
+  console.log(type)
+  markers[type].push(marker);
+
+  // Setup the click event listener for the Marker:
+  // Show the infoWindow with information about the selected marker
+  _m.event.addListener(marker, 'click', function() {
+    infoWindow.close();
+    infoWindow.setContent(description)
+    infoWindow.open(map, marker);
+  });
+
+  // Setup the dragend event listener for the Marker:
+  // Persist the new position in the database
+  _m.event.addListener(marker, 'dragend', function() {
+    // TODO: call backend services - persist new position
+    console.log('marker position changed to:');
+    console.log(marker.getPosition());
+  })
+}
 
 /**
  * Performs additional needed actions when the DOM is loaded
@@ -104,7 +136,7 @@ $(function() {
   });
 
   // Handle filtering of markers via the checkboxes
-  $("#point-types input[type='checkbox']").click(function() {
+  $("#sidebar input[type=checkbox]").click(function() {
     var $this   = $(this);
     var checked = $this.is(':checked');
     var type    = this.value;
@@ -118,5 +150,3 @@ $(function() {
     }
   });
 });
-
-
