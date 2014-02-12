a5aaafcb4d35224b6926a9439a7a93564ad3b72c
Georgi Gardev <georgi@gardev.com>
2014-02-12 22:25:03 +0200

    Remove unnesecary code

--- controllers/json_controller.rb
+++ controllers/json_controller.rb
@@ -4,9 +4,6 @@ module Placify
 
     get '/points.json' do
       content_type :json
-      POI.create(name: 'О шипка 2', type: 'Restaurant', description: "O shipka", lat: 42.294, lng: 23.332)
-      POI.create(name: 'Марешки', type: 'Pharmacy', description: "qkata apteka", lat: 42.244, lng: 23.432)
-      POI.create(name: 'BSD', type: 'Snooker', description: "bilqrdche brat", lat: 42.214, lng: 23.137)
       {markers: POI.all}.to_json
     end
   end
