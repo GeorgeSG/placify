cab4d67c6f1b8c7328b3b0897e508ae15181f257
Georgi Gardev <georgi@gardev.com>
2014-02-12 23:55:03 +0200

    Add categories to fake points

--- database/fake/points.rb
+++ database/fake/points.rb
@@ -1,23 +1,19 @@
-#!/usr/bin/env ruby
-
-require 'rubygems'
-require 'bson'
-require 'faker'
-require 'mongoid'
-
-
-Dir.glob('../../models/**/*.rb').each      { |model| require model }
-Mongoid.load!(File.expand_path("../mongoid.yml", __FILE__), :development)
-
+require './fake_helpers'
 
 POI.delete_all
+
+types = ['pharmacy', 'restaurant', 'hotel', 'billiard', 'snooker', 'casino', 'supermarket', 'cinema', 'bookstore', 'mall']
+
+
 100.times do
   name = Faker::Name.name
   desc = Faker::Lorem.paragraph(10)
+  type = types.sample
   POI.create(
     name: name,
+    type: type,
     lat: Faker::Address.latitude,
     lng: Faker::Address.longitude,
-    description: "<h3>#{name}</h3><p>#{desc}</p>"
+    description: "<h3>#{name}</h3><span>#{type}<p>#{desc}</p>"
   )
 end
