0e81d9dd77fdcc3904d95d0f8776da20e0902361
Georgi Gardev <georgi@gardev.com>
2014-02-12 23:42:33 +0200

    Initial JSON controller

--- controllers/json_controller.rb
+++ controllers/json_controller.rb
@@ -6,5 +6,15 @@ module Placify
       content_type :json
       {markers: POI.all}.to_json
     end
+
+    get '/types.json' do
+      content_type :json
+      {types: POI.all.map(&:type).uniq}.to_json
+    end
+
+    get '/extras.json' do
+      content_type :json
+      {extras: Extra.all.map(*:name).uniq}.to_json
+    end
   end
 end
