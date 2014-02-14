fe6d479ce79d68295a2e5eb58491d84c0b4bee12
Georgi Gardev <georgi@gardev.com>
2014-02-14 23:51:39 +0200

    Add schedule

--- database/fake/points.rb
+++ database/fake/points.rb
@@ -1,16 +1,25 @@
 require './fake_helpers'
 
-POI.delete_all
+Poi.delete_all
+
+beginnings = [0, 600, 800, 900, 1000, 1200]
+endings = [1800, 1900, 2000, 2100]
+
 
 100.times do
   name = Faker::Name.name
   desc = Faker::Lorem.paragraph(10)
-  type = POI.types.sample.to_s
-  POI.create(
+  type = Poi.types.sample.to_s
+  Poi.create(
     name: name,
     type: type,
     lat: Faker::Address.latitude,
     lng: Faker::Address.longitude,
-    description: desc
+    description: desc,
+    schedule: {mon: [beginnings.sample, endings.sample],
+               tue: [beginnings.sample, endings.sample],
+               wed: [beginnings.sample, endings.sample],
+               thu: [beginnings.sample, endings.sample],
+               fri: [beginnings.sample, endings.sample]}
   )
 end
